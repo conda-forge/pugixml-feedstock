fe73505a80418431c7dc4e12b13f65260bf916e4
Uwe L. Korn <xhochy@users.noreply.github.com>
2023-06-06 11:34:31 +0200

    Apply suggestions from code review

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,9 +7,8 @@ cd build
 
 cmake ${CMAKE_ARGS} .. \
  -DCMAKE_BUILD_TYPE=Release \
- -DBUILD_SHARED_LIBS=ON \
- -DBUILD_SHARED_AND_STATIC_LIBS=ON || exit 1
+ -DBUILD_SHARED_LIBS=ON
 
-make -j $CPU_COUNT || exit 1
+make -j $CPU_COUNT
 
 make install
